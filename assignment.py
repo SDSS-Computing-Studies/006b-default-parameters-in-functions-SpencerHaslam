499b002e41dfb455871314381f1eac629ac88e33
SpencerHaslam <71406886+SpencerHaslam@users.noreply.github.com>
2020-10-14 12:10:24 -0700

    rg

--- assignment.py
+++ assignment.py
@@ -2,7 +2,13 @@
 
 import math
 
-def tempConversion():
+def tempConversion(tempnum, tipe):
+    if "F" in tipe:
+        answer = ((tempnum - 32) * (5 / 9))
+    else:
+        answer = (((tempnum * (5 / 9)) + 32))
+    return answer
+
 
 
 def factorPair():
--- task1.py
+++ task1.py
@@ -2,3 +2,4 @@
 
 import assignment
 
+print (tempConversion(30, "F"))
